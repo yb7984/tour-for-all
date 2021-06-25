e47545c59ba0ac2e088f20517983613981e1397f
Sunbao Wu <bobowu@outlook.com>
2021-06-25 08:15:03 -0400

    complete basic functions of tour-for-all-express

---
+++ tour-for-all-express/app.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/** Express app for tour-for-all. */
+
+const express = require("express");
+const cors = require("cors");
+
+const { NotFoundError } = require("./expressError");
+
+const { authenticateJWT } = require("./middleware/auth");
+const authRoutes = require("./routes/auth");
+const usersRoutes = require("./routes/users");
+const toursRoutes = require("./routes/tours");
+const tourPlayersRoutes = require("./routes/tourPlayers");
+
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(authenticateJWT);
+
+app.use("/auth", authRoutes);
+app.use("/users", usersRoutes);
+app.use("/tours", tourPlayersRoutes);
+app.use("/tours", toursRoutes);
+
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
+
+module.exports = app;
---
+++ tour-for-all-express/readme.md
@@ -0,0 +1,11 @@
+# Tour_For_All Backend
+
+This is the Express backend for Tour for all.
+
+To run this:
+
+    node server.js
+    
+To run the tests:
+
+    jest -i
